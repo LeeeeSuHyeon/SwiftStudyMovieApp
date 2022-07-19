e6a417ccb01101ade30340683fcdb4c36ee8fe9d
SuHyeon Lee <tngus0673@naver.com>
2022-07-19 12:42:45 +0900

    Initial Commit

---
+++ Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by 이수현 on 2022/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
