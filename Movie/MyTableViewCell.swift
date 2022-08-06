d67b790c252b68e04add20800ad87dcf586b48db
LeeeeSuHyeon <tngus0673@naver.com>
2022-08-06 19:28:35 +0900

    first commit

---
+++ Movie/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  Movie
+//
+//  Created by 이수현 on 2022/07/20.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Movie/ViewController.swift
+++ Movie/ViewController.swift
@@ -1,19 +1,55 @@
 //
 //  ViewController.swift
 //  Movie
-//
-//  Created by 이수현 on 2022/07/19.
-//
+
 
 import UIKit
+let name = ["aa","bb","cc","dd","ee"]
+// https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=9e8bb4ba542836f87361a78925f7775b&targetDt=20220720
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var table: UITableView!
+    let movieURL = "https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=9e8bb4ba542836f87361a78925f7775b&targetDt=20220720"
     override func viewDidLoad() {
         super.viewDidLoad()
+        table.delegate = self
+        table.dataSource = self
+        getData()
         // Do any additional setup after loading the view.
     }
-
+    func getData(){
+        if let url = URL(string: movieURL){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+            if error != nil{
+                print(error!)
+                return
+            }
+            if let JSONdata = data{
+                //print(JSONdata)
+                let dataSting = String(data: JSONdata, encoding: .utf8)
+                print(dataSting!)
+            }
+        }
+            task.resume()
+    }
+}
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! MyTableViewCell
+        cell.movieName.text = name[indexPath.row]//indexPath.description
+        return cell
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.description)
+    }
 
 }
 
